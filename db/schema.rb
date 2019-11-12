077b3533871aa1bd9a7eee860c808c96376d6312
Noah Thorne <noahlthorne@gmail.com>
2019-11-12 15:47:02 +0900

    I added tiny pictures to each game

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_11_090535) do
+ActiveRecord::Schema.define(version: 2019_11_12_052623) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2019_11_11_090535) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "cover_image"
   end
 
   create_table "listings", force: :cascade do |t|
