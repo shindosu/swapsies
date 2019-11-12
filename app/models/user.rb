e76e36b29f1bed6bd035e8507b5cc6fd40b36407
Noah Thorne <noahlthorne@gmail.com>
2019-11-12 17:15:41 +0900

    added user login functionality and associated views

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :listings
 
   # has_many :offered_swaps, through: :listings, foreign_key: :listing_offered, source: :swaps
-  validates :first_name, :last_name, :username, :city, :photo, :email, presence: true
+  validates :first_name, :last_name, :username, :city, :email, presence: true
   validates :username, :email, uniqueness: true
   validates :email, format: { with: /\A[a-z0-9.]+@[a-z]+\.[a-z]{2,3}\z/i }
   devise :database_authenticatable, :registerable,
