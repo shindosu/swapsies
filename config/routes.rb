5d0b639f18fa7d522588005850034fc14982abd5
bogdanmatei <matei90bogdan@yahoo.com>
2019-11-12 14:31:44 +0900

    fixed typo in routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   end
   resources :swaps, only: [:update, :index]
   resources :users, only: [:show]
-  resources :game, only: [:show]
+  resources :games, only: [:show]
 end
