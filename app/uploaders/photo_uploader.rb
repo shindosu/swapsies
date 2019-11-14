0b26c79ef93742924ccd83528a328ca3ff60121a
Edward Blundell <ewblundell@gmail.com>
2019-11-14 12:09:12 +0900

    Got image upload working and modified navbar and user show image

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,3 +1,25 @@
 class PhotoUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
+
+  process eager: true  # Force version generation at upload time.
+
+  process convert: 'jpg'
+
+  version :thumnail do
+    resize_to_fit 256, 256
+  end
+
+  version :bright_face do
+    cloudinary_transformation effect: "brightness:30", radius: 20,
+      width: 150, height: 150, crop: :thumb, gravity: :face
+  end
+
+  version :user_photo_main do
+    cloudinary_transformation radius: 20,
+      width: 160, height: 160, crop: :thumb, gravity: :face
+  end
+
+  version :dashboard_avatar do
+    cloudinary_transformation gravity: :face, crop: :thumb, width: 40, height: 40, radius: :max
+  end
 end
