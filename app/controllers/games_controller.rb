29b3cf8c5a13142fb9d69f5f9706e506fd217f15
Noah Thorne <noahlthorne@gmail.com>
2019-11-14 15:17:10 +0900

    I added a search with expanded query flexibility

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -5,8 +5,8 @@ class GamesController < ApplicationController
   end
 
   def index
-    if params[:query].present?
-      @games = Game.where("title ILIKE ?", "%#{params[:query]}")
+    if params[:search] && !params[:search][:query].empty?
+      @games = Game.search_by_title_description_console_and_rating(params[:search][:query])
     else
       @games = Game.all.limit(50)
     end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,5 @@
 class Game < ApplicationRecord
+  include PgSearch::Model
   CONSOLES = ['Switch', 'PS4', 'Xbox One', 'PC']
   validates :title, presence: true
   validates :console, uniqueness: {
@@ -12,4 +13,10 @@ class Game < ApplicationRecord
   validates :rating, presence: true
 
   has_many :listings, dependent: :destroy
+
+  pg_search_scope :search_by_title_description_console_and_rating,
+    against: [:title, :description, :console, :rating],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
