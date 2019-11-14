5dc8b718a2b31ecc2297511316dcb43ed3a783e5
bogdanmatei <matei90bogdan@yahoo.com>
2019-11-14 15:14:54 +0900

    created landing page

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,9 @@
 class GamesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
+
+  def home
+  end
+
   def index
     if params[:query].present?
       @games = Game.where("title ILIKE ?", "%#{params[:query]}")
